60ef46ba73f42fbe32a705b086605f285225c202
Blaine <rishi1998@gmail.com>
2021-12-18 14:44:48 +0530

    update values

--- ec2-example/variables.tf
+++ ec2-example/variables.tf
@@ -1,1 +1,18 @@
 # Variable declarations
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "enable_vpn_gateway" {
+  description = "Enable a VPN gateway in your VPC."
+  type        = bool
+  default     = false
+}
